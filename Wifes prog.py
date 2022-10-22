338cf9d4434ac6f0df531dac37a0d2f4b3ad7691
michalkuczynski <mic.kuczynski@gmail.com>
2022-10-22 21:56:06 +0200

    3

---
+++ Wifes prog.py
@@ -0,0 +1,9 @@
+import random
+
+days=["Monday","Tuesday","Wednesday","Thursday","Friday"]
+
+print(f"Group no. 1 : {random.choice(days)} and {random.choice(days)}")
+print(f"Group no. 2 : {random.choice(days)} and {random.choice(days)}")
+print(f"Group no. 3 : {random.choice(days)} and {random.choice(days)}")
+print(f"Group no. 4 : {random.choice(days)} and {random.choice(days)}")
+print(f"Group no. 5 : {random.choice(days)} and {random.choice(days)}")
