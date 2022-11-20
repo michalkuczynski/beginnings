599de5f9388abb52a7361090cb9e0b8133d3aa8f
michalkuczynski <mic.kuczynski@gmail.com>
2022-11-20 22:06:37 +0100

    add 4.3

---
+++ B1_4.3.py
@@ -0,0 +1,11 @@
+def area(a,b):
+    return a*b
+
+def fun():
+    print("This program is counting area of a rectangle")
+    print("Please insert values of sides")
+    a=float(input("a="))
+    b=float(input("b="))
+    print("Area of a rectangle is",area(a,b))
+
+fun()
